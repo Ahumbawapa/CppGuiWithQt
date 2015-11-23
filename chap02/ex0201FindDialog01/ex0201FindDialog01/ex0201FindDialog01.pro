04c634b390e4e1da7d7cd0dac1aa392fc8ad4638
Karsten Kleine <k.kleine@kk2001.net>
2015-11-23 15:15:09 +0100

    Exercises of chap02

---
+++ chap02/ex0201FindDialog01/ex0201FindDialog01/ex0201FindDialog01.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-11-18T15:56:09
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ex0201FindDialog01
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    finddialog.cpp
+
+HEADERS  += \
+    finddialog.h
+
+FORMS    +=
---
+++ chap02/ex0201FindDialog01/ex0201FindDialog01/finddialog.h
@@ -0,0 +1,35 @@
+#ifndef FINDDIALOG_H
+#define FINDDIALOG_H
+
+#include <QDialog>
+
+class QCheckBox;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+
+class FindDialog : public QDialog
+{
+    Q_OBJECT    //Makro necessary for all classes that define SIGNALS and/or SLOTS
+public:
+    FindDialog(QWidget *parent = 0);
+
+signals://Declaration of Signals that the Dialog emits
+    void findNext(const QString &str, Qt::CaseSensitivity cs);
+    void findPrevious(const QString &str, Qt::CaseSensitivity cs);
+
+private slots:
+    void findClicked();
+    void enableFindButton(const QString &text);
+
+private:
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QCheckBox *caseCheckBox;
+    QCheckBox *backwardCheckBox;
+    QPushButton *findButton;
+    QPushButton *closeButton;
+
+};
+
+#endif // FINDDIALOG_H

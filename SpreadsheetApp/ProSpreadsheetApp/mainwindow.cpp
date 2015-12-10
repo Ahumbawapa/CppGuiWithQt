48156bb31863c3f9ba3e6c9127bdbdabc9b78aab
Karsten Kleine <k.kleine@kk2001.net>
2015-12-10 20:09:29 +0100

    Added ctor code to mainwindow.cpp & additional changes from IDE

--- SpreadsheetApp/ProSpreadsheetApp/mainwindow.cpp
+++ SpreadsheetApp/ProSpreadsheetApp/mainwindow.cpp
@@ -1,8 +1,29 @@
+#include <QtGui>
+#include "finddialog.h"         // from chap02
+#include "gotocelldialog.h"     // from chap02
 #include "mainwindow.h"
+#include "sortdialog.h"         // from chap02
+#include "spreadsheet.h"        // from chap04
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    spreadsheet = new Spreadsheet;
+    setCentralWidget(spreadsheet);
+
+    createActions();
+    createMenus();
+    createContextMenu();
+    createToolBars();
+    createStatusBar();
+
+    readSettings();
+
+    findDialog = 0;
+
+    setWindowIcon(QIcon(":images/icon.png"));
+    setCurrentFile("");
 }
 
 MainWindow::~MainWindow()
